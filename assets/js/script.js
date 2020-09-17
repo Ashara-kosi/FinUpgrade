c5aa9f9ebf5b699a57bb8339498cb8e15deab8a2
samuelorobosa <amagbakhensamuel@gmail.com>
2020-09-17 22:01:47 +0100

    added new sections

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+  
+
+$('.owl-carousel').owlCarousel({
+     loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        300:{
+            items:1
+       },
+         1000:{
+            items:3
+        }
+     }
+})
