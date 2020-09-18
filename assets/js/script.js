8643311142dffe740461878799d149ebbcbbc090
samuelorobosa <amagbakhensamuel@gmail.com>
2020-09-18 16:29:09 +0100

    fix: preloader fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let preloaderFunction = () =>{
         preloader.style.display = 'none';   
 }
 
+window.addEventListener('load', preloaderFunction)
 
 $('.owl-carousel').owlCarousel({
      loop:true,
